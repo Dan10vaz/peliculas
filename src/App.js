2ac7ffe1cd84674814c31de54bb554eb774e88df
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-02-16 18:27:46 -0600

    initial commit~

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import PageWrapper from './PageWrapper';
+import Pelicula from './Pelicula';
+import peliculaJson from './peliculas.json';
+import Paginacion from './paginacion';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [paginaActual, setPaginaActual] = useState(1)
+
+  let peliculas = peliculaJson;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+
+              {peliculas.map(pelicula => 
+                <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion} 
+                  director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}
+                  img={pelicula.img}>
+                  {pelicula.descripcion}
+                </Pelicula>
+              )}
+              
+              <Paginacion pagina={paginaActual} total={4} onChange={(pagina) => {
+                setPaginaActual(pagina)
+              }} />
+                
+    </PageWrapper>
   );
 }
-
-export default App;
+        export default App;
